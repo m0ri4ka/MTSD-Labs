75de58b5b6c0d8a6c61749dcece62ae8d79726e9
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2025-05-21 17:12:42 +0300

    Modified equation solver for slightly better readability

--- findEquationRoots.js
+++ findEquationRoots.js
@@ -1,15 +1,11 @@
 const findRoots = (a, b, c) => {
-    let result = [];
     const D = b ** 2 - 4 * a * c;
 
-    if (D == 0) {
-        result = [-b / (2 * a)];
-    } else if (D > 0) {
-        const first = (-b + Math.sqrt(D)) / (2 * a);
-        const second = (-b - Math.sqrt(D)) / (2 * a);
-        result = [first, second];
-    }
-    return result;
+    if (D < 0) return []; 
+    if (D === 0) return [-b / (2 * a)]; 
+
+    const sqrtD = Math.sqrt(D);
+    return [(-b + sqrtD) / (2 * a), (-b - sqrtD) / (2 * a)];
 };
 
 module.exports = findRoots;
