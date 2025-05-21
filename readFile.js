11c70955c89e6605f91adfc30079b7e93e607a04
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2025-05-21 16:59:09 +0300

    Add new imports

---
+++ readFile.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const validate = require('./validation');
+const findRoots = require('./findRoots');
+
+const readFile = (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8').trim();
+        const parts = data.split(' ').map((num) => num.trim());
+
+        if (parts.length !== 3) {
+            throw new Error(`invalid file format`);
+        }
+
+        return parts.map((num, i) => {
+            const { isValid, message } = validate(num, i !== 0);
+            if (isValid) return parseFloat(num);
+            else throw new Error(message);
+        });
+    } catch (error) {
+        console.error(`Error reading file: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+module.exports = readFile;
